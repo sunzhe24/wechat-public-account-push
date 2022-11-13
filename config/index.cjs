3775b0cd8378bcb250ad2fb12527d7dc7fa94de8
孙哲 <118062041+sunzhe24@users.noreply.github.com>
2022-11-13 20:54:03 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ogZwv5pwAvgKkDm-bldABoio9tLw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'J8EopxcZlgvveFiApXnT2SquG7ZQIHdpCtjJ5IJyxQY',
+      useTemplateId: 'LSOgdunHm_wf4VZdBbRmBRTjxjG2YCPJuWLOv3lIbFQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-22',
       festivals: [
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'J8EopxcZlgvveFiApXnT2SquG7ZQIHdpCtjJ5IJyxQY',
+  CALLBACK_TEMPLATE_ID: 'LSOgdunHm_wf4VZdBbRmBRTjxjG2YCPJuWLOv3lIbFQ',
 
   CALLBACK_USERS: [
     {
